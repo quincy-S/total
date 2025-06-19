79effc5c4248ef01095a5de201555834b2ab7421
Quincy <quincy.nbsa@gmail.com>
2025-06-19 23:04:13 +0000

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
 
 
-const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
+const BACKEND_BASE_URL = import.meta.env.VITE_APP_BACKEND_URL || 'http://localhost:5000';
 // Main App component for the user information form
 const App = () => {
   // State variables to store form input values
@@ -135,7 +135,7 @@ const App = () => {
     // console.log(formData);
 
        // In a real application, you would send this formData to your Node.js backend:
-    fetch(`${BACKEND_BASE_URL}/api/data`, {
+    fetch(`${BACKEND_BASE_URL}/data`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
